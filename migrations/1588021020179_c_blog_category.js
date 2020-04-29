0bc1ec20e680a4a0bc6f6a0c483f6303699b69d2
manish_tapadar <manish.tapadar20@gmail.com>
2020-04-29 15:04:39 +0530

    Blog Structure done

--- migrations/1588021020179_c_blog_category.js
+++ migrations/1588021020179_c_blog_category.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "up": "create table if not exists c_blog_category\n" +
-      "(\n" +
-      "\tID varchar(20) not null\n" +
-      "\t\tprimary key,\n" +
-      "\tBLOG_ID varchar(20),\n" +
-      "\tCATREGORY_ID varchar(20),\n" +
-      "\tconstraint c_blog_category_c_category_ID_fk_1\n" +
-      "\t\tforeign key (CATREGORY_ID) references c_category (ID),\n" +
-      "\tconstraint c_blog_category_c_blog_ID_fk\n" +
-      "\t\tforeign key (BLOG_ID) references c_blog (ID)\n" +
-      ");",
+        "up": "create table if not exists c_blog_category\n" +
+          "(\n" +
+          "\tID varchar(20) not null\n" +
+          "\t\tprimary key,\n" +
+          "\tBLOG_ID varchar(20),\n" +
+          "\tCATEGORY_ID varchar(20),\n" +
+          "\tconstraint c_blog_category_c_category_ID_fk_1\n" +
+          "\t\tforeign key (CATEGORY_ID) references c_category (ID),\n" +
+          "\tconstraint c_blog_category_c_blog_ID_fk\n" +
+          "\t\tforeign key (BLOG_ID) references c_blog (ID)\n" +
+          ");",
     "down": "DROP table c_blog_category;"
 }
