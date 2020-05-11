9ad67d3361de5c834deb034ea51ff498f6f10e98
manish_tapadar <manish.tapadar20@gmail.com>
2020-05-11 15:09:55 +0530

    Done publication list

--- model/publicationModel.js
+++ model/publicationModel.js
@@ -57,8 +57,14 @@ exports.getDetailById = async (id) => {
 exports.getUsersPublication = async (userId) => {
   try{
     let data = await knex.from('c_publication_user')
-      .select('c_publication_user.*','c_user.NAME as AUTHOR_FIRST_NAME','c_user.LAST_NAME as AUTHOR_LAST_NAME')
+      .select('c_publication_user.*','c_user.NAME as AUTHOR_FIRST_NAME','c_user.LAST_NAME as AUTHOR_LAST_NAME',
+      'c_publication.TITLE','c_publication.DESCRIPTION','c_publication.TAG_LINE',
+        'AVATAR.PATH as AVATAR_FILE_PATH',
+        'LOGO.PATH as LOGO_FILE_PATH')
       .innerJoin('c_user', 'c_publication_user.USER_ID', 'c_user.ID')
+      .innerJoin('c_publication', 'c_publication_user.PUBLICATION_ID', 'c_publication.ID')
+      .leftJoin('c_file as AVATAR', 'c_publication.AVATAR', 'AVATAR.ID')
+      .leftJoin('c_file as LOGO', 'c_publication.LOGO', 'LOGO.ID')
       .where({'c_publication_user.USER_ID':userId});
     return data;
   }
